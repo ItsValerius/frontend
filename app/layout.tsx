31739ddc23e5d1f288cadbb1e139b0dfab3552db
Valentin <kueck.valentin@googlemail.com>
2022-10-27 07:41:09 +0200

    add next font, changed to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,16 @@
+import localFont from '@next/font/local';
+
+// Font files can be colocated inside of `app`
+const myFont = localFont({ src: '../public/robotocondensed-regular-webfont.woff2' });
+
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <html>
+    <html className={myFont.className}>
       <head></head>
       <body>{children}</body>
     </html>
